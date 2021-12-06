d3b1da4b9bc610322e21a433e631fcb03314a0fe
yangwp <66462732@qq.com>
2021-12-06 20:45:01 +0800

    登录

--- src/utils/request.js
+++ src/utils/request.js
@@ -15,10 +15,10 @@ const service = axios.create({
 // request拦截器
 service.interceptors.request.use(config => {
   // 是否需要设置 token
-  const isToken = (config.headers || {}).isToken === false
+  /* const isToken = (config.headers || {}).isToken === false
   if (getToken() && !isToken) {
     config.headers['Authorization'] = 'Bearer ' + getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
-  }
+  }*/
   // get请求映射params参数
   if (config.method === 'get' && config.params) {
     let url = config.url + '?'
@@ -52,8 +52,7 @@ service.interceptors.request.use(config => {
 // 响应拦截器
 service.interceptors.response.use(res => {
   // 未设置状态码则默认成功状态
-  // const code = res.data.code || 200
-  const code = res.data.code === 20000 ? 200 : res.data.code
+  const code = res.data.code || 200
   // 获取错误信息
   const msg = errorCode[code] || res.data.msg || errorCode['default']
   if (code === 401) {
